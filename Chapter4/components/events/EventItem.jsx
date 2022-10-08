3b7e176feee155d8a7ec9d1e35204b88d03f76ca
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2022-10-08 18:57:55 +0100

    Chapter4: Add event details page

--- Chapter4/components/events/EventItem.jsx
+++ Chapter4/components/events/EventItem.jsx
@@ -1,4 +1,6 @@
-import Link from "next/link";
+import DateIcon from "../icons/DateIcon";
+import AddressIcon from "../icons/AddressIcon";
+import ArrowRightIcon from "../icons/ArrowRightIcon";
 import Button from "../ui/Button";
 import classes from "./EventItem.module.css";
 
@@ -14,19 +16,29 @@ const EventItem = ({ title, image, date, location, id }) => {
 
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} srcset="" />
+      <img src={"/" + image} alt={title} />
+
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
+
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
+
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
