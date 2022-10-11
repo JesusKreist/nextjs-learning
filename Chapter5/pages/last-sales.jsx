4372c138c440d6da37e3113930e7fef059e1dda9
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2022-10-11 08:10:11 +0100

    Chapter5: Add client side data fetching with useSWR hook

---
+++ Chapter5/pages/last-sales.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const LastSales = () => {
+  const [sales, setSales] = useState([]);
+  const dbUrl =
+    "https://nextjs-course-168de-default-rtdb.firebaseio.com/sales.json";
+
+  const { data, error } = useSWR(dbUrl, (url) =>
+    fetch(url).then((res) => res.json())
+  );
+
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch(dbUrl)
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         const transformedSales = [];
+
+  //         for (const key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+  //         setSales(transformedSales);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+
+  if (error) {
+    return <p>Failed to load.</p>;
+  }
+
+  if (!data || sales.length === 0) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default LastSales;
